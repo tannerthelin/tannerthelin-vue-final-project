a1daa9eb9d981976c5eb1f1bbc80e9a6543d6e1d
tannerthelin <tannerthelin@gmail.com>
2019-10-15 22:07:27 -0600

    Added header without interactivity

---
+++ src/components/Header.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="header-container">
+        <div class="logo-container">
+            <img alt="Computer Logo" src="../assets/logo.png" style="width: 62px">
+        </div>
+        <div class="menu-dots"></div>
+        
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+
+    .header-container {
+        display: flex;
+        align-content: center;
+        justify-content: space-between;
+        padding: 40px;
+    }
+
+    .menu-dots {
+        background-image: url("../assets/menu-dots.svg");
+        background-position: center;
+        width: 32px;
+        height: 32px;
+        background-repeat: no-repeat;
+        display: inline-block;
+        cursor: pointer;
+    }
+
+    .menu-dots:hover {
+        background-image: url("../assets/menu-dots-black.svg");
+    }
+</style>
