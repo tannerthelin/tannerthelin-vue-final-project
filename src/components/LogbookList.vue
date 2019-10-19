1aeabfd561b73896e4e121208df562fba8953c3e
tannerthelin <tannerthelin@gmail.com>
2019-10-19 16:36:03 -0600

    Created logged game component, displayed list

--- src/components/LogbookList.vue
+++ src/components/LogbookList.vue
@@ -1,17 +1,45 @@
 <template>
-    <div class="logbook">
-        <h3>My logbook</h3>
+    <div class="logbook-container">
+    <h3>My logbook</h3>
+        <div class="logbook">            
+            <logged-game v-for="game in loggedGames" :game="game" :key="game"></logged-game>
+        </div>
     </div>
 </template>
 
 <script>
+import LoggedGame from "./LoggedGame.vue";
+
 export default {
-    
+    data () {
+        return {
+            loggedGames: [
+                  {title:"Untitled Goose Game", date: 'Jan 8, 2020'},
+                  {title:"Hollow Knight", date: 'Dec 24, 2019'},
+                  {title:"Modern Warfare", date: 'Nov 14, 2019'},
+                  {title:"Fortnite", date: 'Jan 8, 2020'},
+                  {title:"Celeste", date: 'Dec 24, 2019'},
+                  {title:"Destiny 2", date: 'Nov 14, 2019'},
+                  {title:"Link's Awakening", date: 'Jan 8, 2020'},
+                  {title:"Borderlands 3", date: 'Dec 24, 2019'},
+              ]
+        }
+    },
+    components: {
+        loggedGame: LoggedGame
+    }
 }
 </script>
 
 <style>
-    .logbook {
+    .logbook-container {
         margin-top: 80px;
+        padding: 16px;
+    }
+
+    .logbook {
+        display: flex;
+        margin: 0 -13px;
+        flex-wrap: wrap;
     }
 </style>
---
+++ src/components/LoggedGame.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="main-container">
+        <div class="image-container"></div>
+        <h2>{{game.title}}</h2>
+        <h3>Jan 8, 2020</h3>
+        <p>[rating here]</p>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['game']
+}
+</script>
+
+<style scoped>
+    
+    .main-container {
+        width: 132px;
+        margin: 0 13px;
+        margin-bottom: 64px;
+    }
+
+    .image-container {
+        background-image: url('../assets/images/placeholder-large.png');
+        width: 100%;
+        height: 170px;
+        background-size: contain;
+        border-radius: 4px;
+        box-shadow: 0px 11px 14px 0px rgba(0,0,0,0.05);
+        margin-bottom: 12px
+    }
+
+    h3 {
+        margin: 0px;
+        margin-top: 5px;
+    }
+
+    h2 {
+        font-weight: 500;
+    }
+
+</style>
