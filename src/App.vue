7c321da5c1631bd5e8cd4c952b099628954c1a35
tannerthelin <tannerthelin@gmail.com>
2019-11-27 18:46:14 -0700

    Made the global text color a little darker

--- src/App.vue
+++ src/App.vue
@@ -45,7 +45,7 @@ export default {
     font-family: 'Inter', sans-serif; 
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
-    color: #2c3e50;
+    color: #2D2D2D;
     background-color: #F9F9FA;
     min-height: 100vh;
     border: 15px solid white;
